b97ad7e20f224984c5bd063d4320498592896ac6
Ezekiel Adetoro <adetoroe787@gmail.com>
2024-12-07 18:46:18 -0800

    links

--- starter_app/main.py
+++ starter_app/main.py
@@ -5,3 +5,8 @@ app = FastAPI()  # Create an instance of FastAPI
 @app.get("/")  # Define a GET endpoint
 def read_root():
     return {"message": "Hello, World!"}
+
+
+# http://127.0.0.1:8000/docs#/default/read_root__get
+# http://127.0.0.1:8000/openapi.json
+# http://127.0.0.1:8000/
