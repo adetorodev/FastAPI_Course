b769cee5edee19e42e6774b2e16811c6a567d260
Ezekiel Adetoro <adetoroe787@gmail.com>
2024-12-11 20:12:27 -0800

    fast

--- starter_app/main.py
+++ starter_app/main.py
@@ -2,11 +2,26 @@ from fastapi import FastAPI
 
 app = FastAPI()  # Create an instance of FastAPI
 
-@app.get("/")  # Define a GET endpoint
-def read_root():
-    return {"message": "Hello, World!"}
+# @app.get("/")  # Define a GET endpoint
+# def read_root():
+#     return {"message": "Hello, World!"}
 
 
-# http://127.0.0.1:8000/docs#/default/read_root__get
-# http://127.0.0.1:8000/openapi.json
-# http://127.0.0.1:8000/
+# Path parameters are a way to include dynamic values in the
+# URL's path, allowing you to create RESTful APIs that respond
+# based on the values provided in the path
+
+# Defining Path Parameters
+# Type Validation
+# Order of Parameters
+
+items = {
+    1: {"name": "Laptop", "price": 1000},
+    2: {"name": "Phone", "price": 500},
+    3: {"name": "Tablet", "price": 300}
+}
+
+
+@app.get("/items/{item_id}")
+async def get_item(item_id: int):
+    return items[item_id]
