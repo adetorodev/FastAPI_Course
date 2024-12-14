04064828f9f4c414c90b078166b6f263d23ebecf
Ezekiel Adetoro <adetoroe787@gmail.com>
2024-12-14 19:07:26 +0100

    Query parameters

--- starter_app/main.py
+++ starter_app/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
+from typing import List, Optional
+from fastapi import FastAPI, Query
 
 app = FastAPI()  # Create an instance of FastAPI
+
+
 
 # @app.get("/")  # Define a GET endpoint
 # def read_root():
@@ -14,3 +18,24 @@ app = FastAPI()  # Create an instance of FastAPI
 # Validation with Type Hints
 # Default Values
 # Automatic Documentation
+
+@app.get("/")
+async def greet(name: str = "Guest", age: int = None):
+    if age:
+        return {"message": f"Hello, {name}! you are {age} year old"}
+    return {"message": f"Hello, {name}!"}
+
+@app.get("/products/")
+async def filter_products(
+    categories: Optional[List[str]] = Query(None, description="Filter by categories"),
+    price_min: Optional[float] = Query(0.0, description="Minimum price filter"),
+    price_max: Optional[float] = Query(None, description="Maximum price filter")
+):
+    return {
+        "categories": categories,
+        "price_range": {"min": price_min, "max": price_max},
+        "products": [
+            {"product_id": 1, "name": "Laptop", "category": "electronics"},
+            {"product_id": 2, "name": "Smartphone", "category": "electronics"}
+        ]
+    }
