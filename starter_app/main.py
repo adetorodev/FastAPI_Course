a1b4e4401ea354d359422bb405ca315c1db517ad
Ezekiel Adetoro <adetoroe787@gmail.com>
2024-12-20 00:01:31 +0100

    pydantic model

--- starter_app/main.py
+++ starter_app/main.py
@@ -1,44 +1,26 @@
 from fastapi import FastAPI
-from typing import List, Optional
-from fastapi import FastAPI, Query
+from pydantic import BaseModel, EmailStr
 
 app = FastAPI()  # Create an instance of FastAPI
 
-@app.get("/validat-string")
-async def validate_string(
-     username: str = Query(
-        ...,  # Indicates this parameter is required
-        min_length=3,  # Minimum length of 3
-        max_length=15,  # Maximum length of 15
-        regex="^[a-zA-Z0-9_]+$",  # Allows only alphanumeric characters and underscores
-        description="Username must be 3-15 characters long and can only contain letters, numbers, and underscores."
-    )
-):
-    return {"message": f"Validate username: {username}"}
+# Request model definition
+class UserRequest(BaseModel):
+    username: str
+    email: EmailStr
+    age: int
 
-@app.get("/validat-number")
-async def validate_number(
-     age: int = Query(
-        ...,  # Indicates this parameter is required
-        ge=18,  # Minimum value (greater than or equal to 18)
-        le=100,  # Maximum value (less than or equal to 100)
-        description="Age must be between 18 and 100."
-    )
-):
-    return {"message": f"Validated age: {age}"}
+class UserResponse(BaseModel):
+    username: str
+    age: int
 
-@app.get("/validate/")
-async def validate(
-    username: str = Query(..., min_length=3, max_length=15, regex="^[a-zA-Z0-9_]+$"),
-    age: int = Query(..., ge=18, le=100)
-):
-    return {"message": f"Validated username: {username} and age: {age}"}
+@app.post("/register")
+async def register_user(user: UserRequest):
 
-# Parameter	Type	Description
-# min_length	String	Minimum length of a string.
-# max_length	String	Maximum length of a string.
-# regex	String	Regular expression pattern for a string.
-# ge	Number	Greater than or equal to (minimum value).
-# le	Number	Less than or equal to (maximum value).
-# gt	Number	Greater than (exclusive minimum).
-# lt	Number	Less than (exclusive maximum).
+    return {"message": "User registered successfully", "user": user}
+
+
+
+@app.get("/user/{user_id}", response_model=UserResponse)
+async def get_user(user_id: int):
+
+    return {"username": "johndoe", "age": 25}
