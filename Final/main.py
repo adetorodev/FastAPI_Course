f69abd776b309bf9ed74ce9a21b2e572ea3960d0
Ezekiel Adetoro <adetoroe787@gmail.com>
2025-03-08 18:12:56 +0100

    multiple models

--- Final/main.py
+++ Final/main.py
@@ -3,7 +3,7 @@ from fastapi import Depends, FastAPI, HTTPException, Query
 from sqlmodel import Session, SQLModel, select
 from database import engine
 from depedency import get_session, create_db_and_tables
-from models import User
+from models import User, UserPublic, UserUpdate, UserCreate
 
 
 app = FastAPI()
@@ -18,37 +18,78 @@ def on_startup():
 
 
 # Crud operation
-@app.post("/users/")
-def create_user(user: User, session: SessionDep) -> User: # type: ignore
-    session.add(user)
+# @app.post("/users/")
+# def create_user(user: User, session: SessionDep) -> User: # type: ignore
+#     session.add(user)
+#     session.commit()
+#     session.refresh(user)
+#     return user
+
+
+# @app.get("/users/")
+# def read_users(
+#     session: SessionDep, # type: ignore
+#     offset: int = 0,
+#     limit: Annotated[int, Query(le=100)] = 100,
+# ) -> list[User]:
+#     users = session.exec(select(User).offset(offset).limit(limit)).all()
+#     return users
+
+
+# @app.get("/users/{user_id}")
+# def read_user(user_id: int, session: SessionDep) -> "User": # type: ignore
+#     user = session.get(User, user_id)
+#     if not user:
+#         raise HTTPException(status_code=404, detail="user not found")
+#     return user
+
+
+# @app.delete("/users/{user_id}")
+# def delete_user(user_id: int, session: SessionDep): # type: ignore
+#     user = session.get(User, user_id)
+#     if not user:
+#         raise HTTPException(status_code=404, detail="User not found")
+#     session.delete(user)
+#     session.commit()
+#     return {"ok": True}
+
+# ======= Multiple model =========
+
+@app.post("/user/", response_model=UserPublic)
+def create_user(user: UserCreate, session: SessionDep): # type: ignore
+    db_user = User.model_validate(user)
+    session.add(db_user)
     session.commit()
-    session.refresh(user)
-    return user
+    session.refresh(db_user)
+    return db_user
 
 
-@app.get("/users/")
+@app.get("/users/", response_model=list[UserPublic])
 def read_users(
     session: SessionDep, # type: ignore
     offset: int = 0,
     limit: Annotated[int, Query(le=100)] = 100,
-) -> list[User]:
+):
     users = session.exec(select(User).offset(offset).limit(limit)).all()
     return users
 
 
-@app.get("/users/{user_id}")
-def read_user(user_id: int, session: SessionDep) -> "User": # type: ignore
-    user = session.get(User, user_id)
-    if not user:
-        raise HTTPException(status_code=404, detail="user not found")
-    return user
-
-
-@app.delete("/users/{user_id}")
-def delete_user(user_id: int, session: SessionDep): # type: ignore
+@app.get("/users/{user_id}", response_model=UserPublic)
+def read_users(user_id: int, session: SessionDep): # type: ignore
     user = session.get(User, user_id)
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
-    session.delete(user)
+    return user
+
+
+@app.patch("/users/{user_id}", response_model=UserPublic)
+def update_user(user_id: int, user: UserUpdate, session: SessionDep): # type: ignore
+    user_db = session.get(User, user_id)
+    if not user_db:
+        raise HTTPException(status_code=404, detail="User not found")
+    user_data = user.model_dump(exclude_unset=True)
+    user_db.sqlmodel_update(user_data)
+    session.add(user_db)
     session.commit()
-    return {"ok": True}
+    session.refresh(user_db)
+    return user_db
--- Final/models.py
+++ Final/models.py
@@ -1,10 +1,32 @@
 from sqlmodel import Field, Session, SQLModel, create_engine, select
 
 
-class User(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)
+# class User(SQLModel, table=True):
+#     id: int | None = Field(default=None, primary_key=True)
+#     name: str = Field(index=True)
+#     age: int | None = Field(default=None, index=True)
+#     secret_name: str
+
+
+class UserBase(SQLModel):
     name: str = Field(index=True)
     age: int | None = Field(default=None, index=True)
+
+
+class User(UserBase, table=True):
+    id: int | None = Field(default=None, primary_key=True)
     secret_name: str
 
 
+class UserPublic(UserBase):
+    id: int
+
+
+class UserCreate(UserBase):
+    secret_name: str
+
+
+class UserUpdate(UserBase):
+    name: str | None = None
+    age: int | None = None
+    secret_name: str | None = None
